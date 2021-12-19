2e85cc1f74d7917477c915597f0ca9ef13830b64
Irfan Muhit <irfan.muhit85@gmail.com>
2021-12-19 21:00:14 +0600

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+#include <windows.h>
+
+LRESULT CALLBACK myWindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int cmdShow)
+{
+	WNDCLASS wc = { 0 };
+
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursorW(NULL, IDC_ARROW);
+	wc.hInstance = instance;
+	wc.lpszClassName = L"myWindowClass";
+	wc.lpfnWndProc = myWindowProcedure;
+
+	if (!RegisterClassW(&wc))
+	{
+		return -1;
+	}
+
+	CreateWindowW(L"myWindowClass", L"My Window Title", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+
+	MSG msg = { 0 };
+
+	while (GetMessageW(&msg, NULL, NULL, NULL))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK myWindowProcedure(HWND windowHandle, UINT message, WPARAM wp, LPARAM lp)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProcW(windowHandle, message, wp, lp);
+	}
+}
